a31afd200ce25fb0540ad9ef611243d9fccbbd77
vkamalkishore007 <74551567+vkamalkishore007@users.noreply.github.com>
2021-08-01 19:39:21 +0530

    Day 2 Assignment Submitted

---
+++ Day2/Assignment1day2.py
@@ -0,0 +1,5 @@
+#DAY 2 ASSIGNMENT
+#to remove all the occurenece of an elemnt in a list
+li=[1,2,3,2,3,4]
+print(li)
+list(filter(lambda a:a!=2,li))
---
+++ Day2/Assignment2day2.py
@@ -0,0 +1,13 @@
+import string
+def ispangram(str):
+   alphabet = "abcdefghijklmnopqrstuvwxyz"
+   for char in alphabet:
+      if char not in str.lower():
+         return False
+   return True
+
+string = 'The quick brown fox jumps over the lazy dog.'
+if(ispangram(string) == True):
+   print("Y")
+else:
+   print("N")
