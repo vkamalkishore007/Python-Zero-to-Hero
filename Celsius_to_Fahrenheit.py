159e7b838d11151922af5eddcc76d1cb31ce71ea
vkamalkishore007 <74551567+vkamalkishore007@users.noreply.github.com>
2021-08-01 19:40:57 +0530

    Day 4 Project submitted

---
+++ Celsius_to_Fahrenheit.py
@@ -0,0 +1,6 @@
+#Celsius_to_Fahrenheit
+
+celsius = float(input('Enter temperature in Celsius: '))  
+   
+fahrenheit = (celsius * 1.8) + 32  
+print('%0.1f  Celsius is equal to %0.1f degree Fahrenheit'%(celsius,fahrenheit))
